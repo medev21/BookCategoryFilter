a7c40a51dfc2fbc7303fcf8ef4ad558a2304d69c
Martin Ben <mebenav88@gmail.com>
2016-01-02 23:48:59 -0600

    add index new create book controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,28 @@
 class BooksController < ApplicationController
+  before_action :find_book
 
   def index
   end
-  
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :description, :author)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
+    end
 end
